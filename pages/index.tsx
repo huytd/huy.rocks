c1980a5c6910ed2c83724519427b8fc90cfdb939
Huy <kingbazoka@gmail.com>
2021-12-31 12:43:07 -0800

    Add everyday project

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,7 @@ const Home: NextPage = () => {
       <h2>Development Log</h2>
       <p>{"I keep a development log for some of the project I'm working on, you can find them here:"}</p>
       <ul>
+        <li>-&gt; <Link href="/everyday"><a className="font-bold">Everyday Learning</a></Link>: <i>I am trying to learn something everyday and write about it.</i></li>
         <li>-&gt; <Link href="/ascii-d"><a className="font-bold">ASCII-d</a></Link>: <i>Cross-platform ASCII diagram drawing application</i></li>
         <li>-&gt; <Link href="/snarkyterm"><a className="font-bold">SnarkyTerm</a></Link>: <i>A terminal emulator written in Rust and WGPU</i></li>
         <li>-&gt; <Link href="/web-debugger"><a className="font-bold">Web Debugger</a></Link>: <i>A Web-based JavaScript debugger</i></li>
