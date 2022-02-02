755364bf0d97136cf5e8cba68c437c7efe3d053c
Huy <kingbazoka@gmail.com>
2022-02-01 18:11:48 -0800

    Implement cache for image generator

--- pages/api/image.ts
+++ pages/api/image.ts
@@ -2,6 +2,8 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { createCanvas, GlobalFonts, SKRSContext2D } from '@napi-rs/canvas';
 import { base64_decode } from '../../utils/base64';
 import { resolve } from 'path';
+import cache from '../../utils/middleware/cache';
+import LRUCache from 'lru-cache';
 
 const IMG_WIDTH = 1200;
 const IMG_HEIGHT = 600;
@@ -29,9 +31,10 @@ function wrapText(context: SKRSContext2D, text: string, x: number, y: number, ma
 GlobalFonts.registerFromPath(resolve('./public', 'fonts', 'Inter-Regular.ttf'));
 GlobalFonts.registerFromPath(resolve('./public', 'fonts', 'Inter-Bold.ttf'));
 
-export default function handler(
+function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
+  lruCache: LRUCache<string, Buffer>
 ) {
   let input = "";
   const inputs = req.query["t"] ?? "";
@@ -40,36 +43,46 @@ export default function handler(
   } else {
     input = inputs;
   }
-  let [date, title] = base64_decode(input).split(" - ");
-  date = date || "huy.rocks";
-  title = title || "everyday learning";
+  let imageData = null;
+  if (lruCache.has(input)) {
+    imageData = lruCache.get(input);
+  } else {
+    let [date, title] = base64_decode(input).split(" - ");
+    date = date || "huy.rocks";
+    title = title || "everyday learning";
 
-  const canvas = createCanvas(IMG_WIDTH, IMG_HEIGHT);
-  const ctx = canvas.getContext("2d");
+    const canvas = createCanvas(IMG_WIDTH, IMG_HEIGHT);
+    const ctx = canvas.getContext("2d");
 
-  ctx.imageSmoothingEnabled = true;
-  ctx.imageSmoothingQuality = 'high';
+    ctx.imageSmoothingEnabled = true;
+    ctx.imageSmoothingQuality = 'high';
 
-  ctx.fillStyle = '#F7F8FB';
-  ctx.fillRect(0, 0, IMG_WIDTH, IMG_HEIGHT);
+    ctx.fillStyle = '#F7F8FB';
+    ctx.fillRect(0, 0, IMG_WIDTH, IMG_HEIGHT);
 
-  ctx.font = "bold 35px Inter";
-  ctx.fillStyle = '#253A59';
-  ctx.fillText("->", 70, 95);
-  ctx.fillText("/huy/rocks", 216, 95);
+    ctx.font = "bold 35px Inter";
+    ctx.fillStyle = '#253A59';
+    ctx.fillText("->", 70, 95);
+    ctx.fillText("/huy/rocks", 216, 95);
 
-  ctx.font = "35px Inter";
-  ctx.fillStyle = '#AAB7CA';
-  ctx.fillText("/home", 111, 95);
+    ctx.font = "35px Inter";
+    ctx.fillStyle = '#AAB7CA';
+    ctx.fillText("/home", 111, 95);
 
-  ctx.font = "bold 48px Inter";
-  ctx.fillStyle = '#AAB7CA';
-  ctx.fillText(date, 70, 290);
+    ctx.font = "bold 48px Inter";
+    ctx.fillStyle = '#AAB7CA';
+    ctx.fillText(date, 70, 290);
 
-  ctx.font = "bold 72px Inter";
-  ctx.fillStyle = '#253A59';
-  wrapText(ctx, title, 70, 370, IMG_WIDTH - 80, 82);
+    ctx.font = "bold 72px Inter";
+    ctx.fillStyle = '#253A59';
+    wrapText(ctx, title, 70, 370, IMG_WIDTH - 80, 82);
+
+    imageData = canvas.toBuffer('image/png');
+    lruCache.set(input, imageData);
+  }
 
   res.setHeader('Content-Type', 'image/png');
-  res.send(canvas.toBuffer('image/png'));
+  res.send(imageData);
 }
+
+export default cache(handler);
