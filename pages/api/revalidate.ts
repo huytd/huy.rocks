8311f4a7a96d82446ca50e59a102f6dd9d65d023
Huy <kingbazoka@gmail.com>
2022-02-17 09:56:58 -0800

    Revalidate any path

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,8 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const path = req.query['path'];
     try {
-        await res.unstable_revalidate('/everyday');
+        let pathToRevalidate = '/everyday';
+        if (typeof path === "string") {
+            pathToRevalidate = path;
+        }
+        await res.unstable_revalidate(pathToRevalidate);
         return res.json({ revalidated: true });
     } catch(err) {
         res.status(500).json({ error: 'Revalidation failed' });
