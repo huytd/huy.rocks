982dd950bc04cc492b3f9525cc71a810522f52a9
Huy <kingbazoka@gmail.com>
2022-03-22 16:57:52 -0700

    Switch to serif font for body text

--- pages/[repo]/[[...subpath]].tsx
+++ pages/[repo]/[[...subpath]].tsx
@@ -47,7 +47,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
     if (matchedIndex !== -1) {
       // Found the matched post
       const matchedDay = days[matchedIndex];
-      markdown = `[<- All posts](/${repo})\n\n# ${matchedDay!.title}\n\n${matchedDay!.tokens.join("")}`;
+      markdown = `[<span class="arrow">&lt;-</span> All posts](/${repo})\n\n# ${matchedDay!.title}\n\n${matchedDay!.tokens.join("")}`;
       const otherStart = Math.max(matchedIndex - 3, 0);
       const otherEnd = otherStart + 6;
       let linkToOthers = days.slice(otherStart, otherEnd).filter(day => day?.slug !== matchedDay?.slug);
@@ -169,7 +169,7 @@ const Devlog: NextPage = ({ markdown, postTitle, repo, subpath }: InferGetStatic
       },
       link(href: string, title: string, text: string) {
           if (href.startsWith("http") && href.indexOf(SITE_URL) === -1)  {
-              return `<a class="external-link" href='${href}' target="_blank" rel="noopener">${text} ↗</a>`;
+              return `<a class="external-link" href='${href}' target="_blank" rel="noopener">${text} <span class="arrow">↗</span></a>`;
           }
           return false;
       }
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,17 +14,17 @@ const Home: NextPage = () => {
                 <h2>Contact</h2>
                 <p>{"In case you're interested:"}</p>
                 <ul>
-                    <li>-&gt; <Link href="https://github.com/huytd">Find me on GitHub</Link></li>
-                    <li>-&gt; <Link href="mailto:hey@huy.rocks">Contact me</Link></li>
+                    <li><span className="arrow">-&gt;</span> <Link href="https://github.com/huytd">Find me on GitHub</Link></li>
+                    <li><span className="arrow">-&gt;</span> <Link href="mailto:hey@huy.rocks">Contact me</Link></li>
                 </ul>
 
                 <h2>Development Log</h2>
                 <p>{"I keep a development log for some of the project I'm working on, you can find them here:"}</p>
                 <ul>
-                    <li>-&gt; <Link href="/everyday"><a className="font-bold">Everyday Learning</a></Link>: <i>I am trying to learn something everyday and write about it.</i></li>
-                    <li>-&gt; <Link href="/ascii-d"><a className="font-bold">ASCII-d</a></Link>: <i>Cross-platform ASCII diagram drawing application</i></li>
-                    <li>-&gt; <Link href="/snarkyterm"><a className="font-bold">SnarkyTerm</a></Link>: <i>A terminal emulator written in Rust and WGPU</i></li>
-                    <li>-&gt; <Link href="/web-debugger"><a className="font-bold">Web Debugger</a></Link>: <i>A Web-based JavaScript debugger</i></li>
+                    <li><span className="arrow">-&gt;</span> <Link href="/everyday"><a className="font-bold">Everyday Learning</a></Link>: <i>I am trying to learn something everyday and write about it.</i></li>
+                    <li><span className="arrow">-&gt;</span> <Link href="/ascii-d"><a className="font-bold">ASCII-d</a></Link>: <i>Cross-platform ASCII diagram drawing application</i></li>
+                    <li><span className="arrow">-&gt;</span> <Link href="/snarkyterm"><a className="font-bold">SnarkyTerm</a></Link>: <i>A terminal emulator written in Rust and WGPU</i></li>
+                    <li><span className="arrow">-&gt;</span> <Link href="/web-debugger"><a className="font-bold">Web Debugger</a></Link>: <i>A Web-based JavaScript debugger</i></li>
                 </ul>
                 <p>{"Most of my other projects doesn't have a DEVLOG, but please feel free to check them on GitHub."}</p>
             </main>
