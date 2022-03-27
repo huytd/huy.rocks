890780436863708af3fe9204ee6a2d9050ff6716
Huy <kingbazoka@gmail.com>
2022-03-27 10:49:30 -0700

    Chagne the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,22 +11,16 @@ const Home: NextPage = () => {
             <main className="container-center github-theme no-list my-10 min-h-full flex-1">
                 <p>{"Hello! I'm Huy! Look like you've found my space on the internet."}</p>
 
-                <h2>Contact</h2>
-                <p>{"In case you're interested:"}</p>
-                <ul>
-                    <li><Link href="https://github.com/huytd">Find me on GitHub</Link></li>
-                    <li><Link href="mailto:hey@huy.rocks">Contact me</Link></li>
-                </ul>
-
-                <h2>Development Log</h2>
                 <p>{"I keep a development log for some of the project I'm working on, you can find them here:"}</p>
-                <ul>
+                <ol>
                     <li><Link href="/everyday"><a className="font-bold">Everyday Learning</a></Link>: <i>Writing about what I learned everyday</i></li>
                     <li><Link href="/ascii-d"><a className="font-bold">ASCII-d</a></Link>: <i>Cross-platform ASCII diagram drawing application</i></li>
                     <li><Link href="/snarkyterm"><a className="font-bold">SnarkyTerm</a></Link>: <i>A terminal emulator written in Rust and WGPU</i></li>
                     <li><Link href="/web-debugger"><a className="font-bold">Web Debugger</a></Link>: <i>A Web-based JavaScript debugger</i></li>
-                </ul>
+                </ol>
                 <p>{"Most of my other projects doesn't have a DEVLOG, but please feel free to check them on GitHub."}</p>
+
+                <p>{"In case you're interested, you can reach me "}<b><Link href="mailto:hey@huy.rocks">via email</Link></b> or <b><Link href="https://github.com/huytd">visit my GitHub</Link></b></p>
             </main>
         </>
     )
