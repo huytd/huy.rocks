f7626eb22a5bf9a046d401f87141fb1aee97ab58
Huy <kingbazoka@gmail.com>
2023-01-05 09:08:30 -0800

    ...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={""}/>
 			<link href="https://fonts.googleapis.com/css2?family=Overpass+Mono&family=Rozha+One&family=Spectral:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
 			<link rel="stylesheet" href="https://use.typekit.net/nme2fxj.css"/>
-			<script src="https://cdn.counter.dev/script.js" data-id="1f306b24-fb3c-4bcc-8057-c8bdf6414783" data-utcoffset="-8"></script>
+			<script src="https://cdn.counter.dev/script.js" dataId="1f306b24-fb3c-4bcc-8057-c8bdf6414783" dataUtcoffset="-8"></script>
         </Head>
         <div className="flex justify-center items-center bg-stone-100 h-16 font-serif">
             <div className="container-center center-horizontal text-stone-500 font-bold font-mono text-sm">
